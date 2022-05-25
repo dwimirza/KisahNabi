824cc0ae5568b78f763abf43a9d93d856c3806ed
anandadwimirza <ananda120206@gmail.com>
2022-05-25 12:31:55 +0700

    Update Glide

--- app/src/main/java/com/nanda/kisahnabi/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/nanda/kisahnabi/ui/detail/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.nanda.kisahnabi.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.nanda.kisahnabi.R
 import com.nanda.kisahnabi.data.KisahResponse
 import com.nanda.kisahnabi.databinding.ActivityDetail2Binding
@@ -15,14 +16,16 @@ class DetailActivity : AppCompatActivity() {
         _binding = ActivityDetail2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         val data = intent.getParcelableExtra<KisahResponse>(DATA)
-        binding.apply {
-            detailNama.text = data?.name
-            detailTahun.text = data?.thnKelahiran
-            detailTempat.text = data?.tmp
-            detailUsia.text = data?.usia
-            detailDesk.text = data?.description
-
-        }
+       data?.let {
+           binding.apply {
+               detailNama.text = data.name
+               detailTahun.text = data.thnKelahiran
+               detailTempat.text = data.tmp
+               detailUsia.text = data.usia
+               detailDesk.text = data.description
+               Glide.with(this@DetailActivity).load(data.imageUrl).into(detailImage)
+           }
+       }
     }
 
     companion object{
